a1563c18c17434a00c7ebfc14ee576b40e4b7a4b
Anshida-Ansari <anshidaansari9a@gmail.com>
2025-04-23 21:09:36 +0530

    stack and queue

--- week 2/queue.js
+++ week 2/queue.js
@@ -77,56 +77,145 @@
 
 
 
-class Queue {
+// class Queue {
+//     constructor(){
+//         this.items = {}
+//         this.head = 0
+//         this.tail = 0
+
+//     }
+
+//     enqueue(elements){
+//         this.items[this.tail] = elements
+//         this.tail++
+
+//     }
+//     dequeue(){
+//         const item = this.items[this.head]
+//         delete this.items[this.head]
+//         this.head++
+//     }
+
+//     isEmpty(){
+//         return this.tail - this.head === 0
+//     }
+
+//     size(){
+//         return this.tail - this.head
+//     }
+
+//     peek(){
+
+//         return this.items[this.head]
+
+//     }
+//     print(){
+//         console.log(this.items);
+        
+//     }
+// }
+
+
+// const q = new Queue()
+
+// q.enqueue(19)
+// q.enqueue(45)
+// q.enqueue(59)
+
+// q.print()
+
+// console.log(q.peek());
+
+
+// q.dequeue()
+// q.print()
+
+//**queue implemented using array */
+
+    class queue{
+        constructor(){
+            this.item = []      
+        }
+
+        enqueue(element){
+            this.item.push(element)
+        }
+
+        dequeue(){
+            this.item.shift()
+        }
+
+        peek(){
+            return this.item[0]
+        }
+
+        size(){
+            return this.item.length
+        }
+        isEmpty(){
+            return this.item.length === 0
+        }
+        print(){
+            console.log(this.item.toString());
+            
+        }
+    }
+
+    const q = new queue()
+
+    q.enqueue(89)
+    q.enqueue(19)
+    console.log(q.isEmpty());
+    console.log(q.size());
+    console.log(q.peek());
+
+
+
+    q.print()
+
+//**stack with object */
+
+class Queue{
     constructor(){
-        this.items = {}
+        this.item = {}
         this.head = 0
         this.tail = 0
 
     }
 
-    enqueue(elements){
-        this.items[this.tail] = elements
+    enqueue(element){
+        this.item[this.tail] = element
         this.tail++
-
     }
     dequeue(){
-        const item = this.items[this.head]
-        delete this.items[this.head]
-        this.head++
+        const items = this.item[this.head]
+        delete this.item[this.head]
+        this.head--
     }
-
-    isEmpty(){
-        return this.tail - this.head === 0
-    }
-
-    size(){
-        return this.tail - this.head
-    }
-
     peek(){
-
-        return this.items[this.head]
-
+        return this.item[this.head]
+    }
+    size(){
+        return  this.tail - this.head
+    }
+    isEmpty(){
+        return this.size() === 0
     }
     print(){
-        console.log(this.items);
+        console.log(this.item);
         
     }
 }
 
+const qu = new Queue()
+qu.enqueue(87)
+qu.enqueue(65)
+qu.enqueue(56)
+qu.dequeue()
+console.log(qu.size());
 
-const q = new Queue()
+console.log(qu.isEmpty());
 
-q.enqueue(19)
-q.enqueue(45)
-q.enqueue(59)
+qu.print()
 
-q.print()
-
-console.log(q.peek());
-
-
-q.dequeue()
-q.print()
 
--- week 2/stack.js
+++ week 2/stack.js
@@ -1,123 +1,179 @@
 // //**stack using array */
 
 
-// class Stack {
-//     constructor(){
-//         this.stack = []
-//     }
-//     push(element){
-//         this.stack.push(element)
-//     }
+class Stack {
+    constructor(){
+        this.stack = []
+    }
+    push(element){
+        this.stack.push(element)
+    }
 
-//     pop(){
-//         if(this.isEmpty()){
-//             return 'stack underflow'
-//         }
+    pop(){
+        if(this.isEmpty()){
+            return 'stack underflow'
+        }
 
-//         return  this.stack.pop()
-//     }
+        return  this.stack.pop()
+    }
 
-//     peek(){
-//         return this.stack[this.stack.length-1]
-//     }
-//     isEmpty(){
-//         return this.stack.length === 0
-//     }
-//     size(){
-//         return this.stack.length
-//     }
-//     print(){
-//         console.log(this.stack.join('<-'));
+    peek(){
+        return this.stack[this.stack.length-1]
+    }
+    isEmpty(){
+        return this.stack.length === 0
+    }
+    size(){
+        return this.stack.length
+    }
+    print(){
+        console.log(this.stack.join('<-'));
         
-//     }
+    }
     
-// }
+}
 
-// const s = new Stack()
-// console.log(s.isEmpty());
+const s = new Stack()
+console.log(s.isEmpty());
 
 
-// s.push(49)
-// s.push(59)
-// s.push(39)
-// s.push(19)
-// s.push(89)
-// s.print()
+s.push(49)
+s.push(59)
+s.push(39)
+s.push(19)
+s.push(89)
+s.print()
 
-// console.log(s.size());
-// console.log(s.isEmpty());
-// console.log(s.peek());
-// s.pop()
-// s.print()
-// console.log(s.pop());
+console.log(s.size());
+console.log(s.isEmpty());
+console.log(s.peek());
+s.pop()
+s.print()
+console.log(s.pop());
 
 
 
 //**stack using linked list */
 
 
-class Node{
-    constructor(value){
-        this.value = value
-        this.next = null
-    }
-}
+// class Node{
+//     constructor(value){
+//         this.value = value
+//         this.next = null
+//     }
+// }
 
 
-class Stack {
-    constructor(){
-        this.top = null
-        this.length = 0
-    }
+// class Stack {
+//     constructor(){
+//         this.top = null
+//         this.length = 0
+//     }
 
-    push(value){
-        const newNode = new Node(value)
-        newNode.next = this.top
-        this.top = newNode
-        this.length++
-    }
-    pop(){
-        if(this.isEmpty()){
-            return 'stack is underflow'
-        }
-        const popped = this.top
-        this.top = this.top.next
-        this.length--
-        return popped.value
-    }
-    peek(){
-        return this.top.value
-    }
+//     push(value){
+//         const newNode = new Node(value)
+//         newNode.next = this.top
+//         this.top = newNode
+//         this.length++
+//     }
+//     pop(){
+//         if(this.isEmpty()){
+//             return 'stack is underflow'
+//         }
+//         const popped = this.top
+//         this.top = this.top.next
+//         this.length--
+//         return popped.value
+//     }
+//     peek(){
+//         return this.top.value
+//     }
 
-    isEmpty(){
-        return this.length === 0
-    }
+//     isEmpty(){
+//         return this.length === 0
+//     }
 
-    size(){
-        return this.length
-    }
+//     size(){
+//         return this.length
+//     }
 
-    print(){
-        let curr = this.top
-        let values = ''
-        while(curr){
-            values += curr.value+ " <- "
-            curr = curr.next
-        }
-        console.log(values + ' null');
+//     print(){
+//         let curr = this.top
+//         let values = ''
+//         while(curr){
+//             values += curr.value+ " <- "
+//             curr = curr.next
+//         }
+//         console.log(values + ' null');
         
+//     }
+// }
+
+
+
+// const s = new Stack()
+
+// s.push(89)
+// s.push(89)
+// s.push(89)
+// console.log(s.isEmpty());
+// console.log(s.size());
+
+
+// s.print()
+
+
+//**stack reverse */
+
+
+function reverese(string){
+    let stack = new Stack()
+
+    for(let char of string){
+        stack.push(char)
     }
+
+    let reveresed = ''
+    while(!stack.isEmpty()){
+        reveresed += stack.pop()
+    }
+
+    return reveresed
+    
 }
 
 
-
-const s = new Stack()
-
-s.push(89)
-s.push(89)
-s.push(89)
-console.log(s.isEmpty());
-console.log(s.size());
+console.log(reverese('anshida'));
 
 
-s.print()
+function isValidParentheses(str){
+
+    let stack =[]
+    let map ={
+        '}':'{',
+        ']':'[',
+        ')':'('
+    }
+
+    for(let char of str){
+        if(char === '[' || char === '{' || char === '('){
+            stack.push(char)
+        
+        }else{
+            if(stack.pop()!== map[char]){
+                return false
+            }
+            
+        }
+
+    }
+    return true
+}
+
+
+console.log(isValidParentheses('{}}{'));
+console.log(isValidParentheses('{}'));
+console.log(isValidParentheses('{}}{'));
+
+
+
